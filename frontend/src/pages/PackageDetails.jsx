76f29ee6b140a58f5d41c69c87e8b1a32f44811c
john-git7 <gideonebenezer467@gmail.com>
2025-11-06 16:25:37 +0530

    :)

--- frontend/src/pages/PackageDetails.jsx
+++ frontend/src/pages/PackageDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { apiRequest } from "../api";
+import { API_URL } from "../config";
 import {
   Card,
   CardHeader,
